03447cc93913b32ebebd3870cc8b0856774ee88a
Rahul Bhati <rahulbhati0202@gmail.com>
2025-05-05 20:57:46 +0530

    add the icon

--- components/AboutData/About.js
+++ components/AboutData/About.js
@@ -3,21 +3,21 @@ import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
 const About = () => {
-  const {colors}=useTheme()
+  const { colors } = useTheme()
   return (
-   <View style={{backgroundColor:colors.background}}>
-    <Text style={[styles.text,{color:colors.text}]}>About Screen</Text>
-   </View>
+    <View style={{ backgroundColor: colors.background }}>
+      <Text style={[styles.text, { color: colors.text }]}>About Screen</Text>
+    </View>
   )
 }
 
-const styles=StyleSheet.create({
-    text:{
-        textAlign: "center",
-        fontSize: 20,
-        margin: 20,
-        fontFamily:"italic"
-    }
+const styles = StyleSheet.create({
+  text: {
+    textAlign: "center",
+    fontSize: 20,
+    margin: 20,
+    fontFamily: "italic"
+  }
 })
 
 export default About
